0e175898cbcdcdc88dc67c5d17511e2c77b2a3fd
johnrearden <isnocturnal@gmail.com>
2024-04-05 13:02:35 +0000

    maint: update workspace to use Gitpod

--- os_messing.py
+++ os_messing.py
@@ -1,8 +1,18 @@
 import os
 import json
+from pathlib import Path
 
-entries = os.scandir()
-for entry in entries:
-    print(entry)
+from datetime import datetime
 
+basepath = Path('')
+
+def convert_date(timestamp):
+    d = datetime.utcfromtimestamp(timestamp)
+    formatted_date = d.strftime('%d %b %Y, %H:%M:%S')
+    return formatted_date
+
+with os.scandir() as dir_contents:
+    for entry in dir_contents:
+        info = entry.stat()
+        print(f'{entry.name}, last modified on {convert_date(info.st_mtime)}')
 
